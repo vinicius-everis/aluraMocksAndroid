a909b889a07acc2bc04b86fd99c7f5c6541033a8
vinicius-everis <vinicius.sferreira@everis.nttdata.com>
2021-10-13 16:21:06 -0300

    Curso de Mocks no Android

---
+++ app/src/test/java/br/com/alura/leilao/ui/AtualizadorDeUsuarioTest.java
@@ -0,0 +1,40 @@
+package br.com.alura.leilao.ui;
+
+import static org.mockito.Mockito.*;
+
+import android.support.v7.widget.RecyclerView;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import br.com.alura.leilao.database.dao.UsuarioDAO;
+import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.ui.recyclerview.adapter.ListaUsuarioAdapter;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AtualizadorDeUsuarioTest {
+
+    @Mock
+    private UsuarioDAO dao;
+    @Mock
+    private ListaUsuarioAdapter adapter;
+    @Mock
+    private RecyclerView recyclerView;
+
+    @Test
+    public void deve_AtualizarListaDeUsuarios_QuandoSalvarUsuario() {
+        AtualizadorDeUsuario atualizador = new AtualizadorDeUsuario(dao, adapter, recyclerView);
+        Usuario alex = new Usuario("Alex");
+        when(dao.salva(alex)).thenReturn(new Usuario(1, "Alex"));
+        when(adapter.getItemCount()).thenReturn(1);
+
+        atualizador.salva(alex);
+
+        verify(dao).salva(new Usuario("Alex"));
+        verify(adapter).adiciona(new Usuario(1, "Alex"));
+        verify(recyclerView).smoothScrollToPosition(0);
+    }
+}
---
+++ app/src/test/java/br/com/alura/leilao/ui/recyclerview/adapter/ListaLeilaoAdapterTest.java
@@ -0,0 +1,44 @@
+package br.com.alura.leilao.ui.recyclerview.adapter;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+
+import android.content.Context;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import br.com.alura.leilao.model.Leilao;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ListaLeilaoAdapterTest {
+
+    @Mock
+    private Context context;
+    @Spy
+    private ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(context);
+
+    @Test
+    public void deve_AtualizarListaDeLeiloes_QuandoReceberListaDeLeiloes() {
+        doNothing().when(adapter).atualizaLista();
+
+        adapter.atualiza(new ArrayList<>(Arrays.asList(
+                new Leilao("Console"),
+                new Leilao("Computador")
+        )));
+
+        int quantidadeLeiloesDevolvida = adapter.getItemCount();
+
+        verify(adapter).atualizaLista();
+
+        assertThat(quantidadeLeiloesDevolvida, is(2));
+    }
+}
